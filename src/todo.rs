76a500b448072ed733ba97096916622036c72446
Joey Malvinni <76851062+joeymalvinni@users.noreply.github.com>
2023-10-20 19:32:43 -0700

    add saving

--- src/todo.rs
+++ src/todo.rs
@@ -1,18 +1,13 @@
 use std::fs;
 
+use crate::ui::Tabs;
+
 pub struct Todo {
     todo: Vec<String>,
     done: Vec<String>,
 }
 
 impl Todo {
-    pub fn new() -> Todo {
-        Todo {
-            todo: vec![],
-            done: vec![],
-        }
-    }
-
     // creates todo struct from file
     pub fn from(file_name: &str) -> Todo {
         let content = fs::read_to_string(file_name).expect("Error finding file.");
@@ -20,15 +15,14 @@ impl Todo {
 
         let mut todo_vec: Vec<String> = vec![];
         let mut done_vec: Vec<String> = vec![];
-        
-        
+
         for line in lines.into_iter() {
             if line.to_lowercase().starts_with("done: ") {
                 todo_vec.push(line[6..].trim().to_string());
             } else if line.to_lowercase().starts_with("todo: ") {
                 done_vec.push(line[6..].trim().to_string());
             }
-        };
+        }
 
         Todo {
             todo: todo_vec,
@@ -65,6 +59,15 @@ impl Todo {
         self.add_to_todo(element);
     }
 
+    // convert position from All tab to either Todo or Done and the relative position
+    pub fn all_to_single(&self, position: usize) -> (usize, Tabs) {
+        if position < self.todo.len() {
+            (position, Tabs::Todo)
+        } else {
+            (position - self.todo.len(), Tabs::Done)
+        }
+    }
+
     // returns a String of the done elements
     pub fn collect_done_md(&mut self) -> Vec<String> {
         self.done
@@ -86,5 +89,26 @@ impl Todo {
         let mut list = self.collect_todo_md();
         list.append(&mut self.collect_done_md());
         list
+    }
+
+    pub fn save(&mut self, file_name: &str) {
+        let mut result = self
+            .todo
+            .as_mut_slice()
+            .into_iter()
+            .map(|x| format!("TODO: {}", x))
+            .collect::<Vec<_>>()
+            .join("\n");
+
+        result += "\n";
+        result += &self
+            .done
+            .as_mut_slice()
+            .into_iter()
+            .map(|x| format!("DONE: {}", x))
+            .collect::<Vec<_>>()
+            .join("\n");
+
+        fs::write(file_name, result).expect("Unable to write file");
     }
 }
