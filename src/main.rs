80f94bcc29b4499b4440446713f90e6727f86070
Joey Malvinni <76851062+joeymalvinni@users.noreply.github.com>
2023-10-20 19:34:06 -0700

    more sophisticated inserting todo elements

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,43 @@
-use crossterm::cursor::{SetCursorStyle, MoveTo};
+use crossterm::cursor::{MoveTo, SetCursorStyle};
+use crossterm::event::{poll, read, Event, KeyCode, KeyEvent, KeyEventKind, KeyModifiers};
 use crossterm::style::Print;
-use crossterm::{cursor, execute, terminal, event};
-use crossterm::event::{poll, read, Event, KeyCode, KeyEvent, KeyModifiers, KeyEventKind};
+use crossterm::{cursor, execute, terminal};
 
-use crossterm::terminal::{disable_raw_mode, enable_raw_mode, EnterAlternateScreen, LeaveAlternateScreen};
-use std::{time::Duration, io::stdout};
+use crossterm::terminal::{
+    disable_raw_mode, enable_raw_mode, EnterAlternateScreen, LeaveAlternateScreen,
+};
+use std::io::Stdout;
+use std::{io::stdout, time::Duration};
 
-mod ui;
 mod todo;
+mod ui;
 use todo::Todo;
-use ui::{UI, Input, Tabs};
+use ui::{Input, Tabs, UI};
+
+const FILE: &str = "TODO.txt";
+
+fn clear_and_render(stdout: &mut Stdout, len: usize, item: &String) {
+    execute!(
+        stdout,
+        MoveTo(0, len as u16 + 2),
+        terminal::Clear(terminal::ClearType::FromCursorDown)
+    )
+    .unwrap();
+    execute!(stdout, MoveTo(1, len as u16 + 2), Print("- [ ]")).unwrap();
+    execute!(stdout, MoveTo(7, len as u16 + 2), Print(item.to_string())).unwrap();
+}
+
+fn remove_last_word(input: &str) -> String {
+    let mut words = input.rsplit(' ').collect::<Vec<&str>>();
+
+    if words.len() < 2 {
+        return String::from("");
+    }
+
+    words.remove(0);
+
+    words.into_iter().rev().collect::<Vec<&str>>().join(" ")
+}
 
 fn main() {
     let mut stdout = stdout();
@@ -17,7 +45,7 @@ fn main() {
     execute!(stdout, EnterAlternateScreen, cursor::MoveTo(0, 0)).unwrap();
     execute!(stdout, cursor::Hide).unwrap();
 
-    let mut todo = Todo::from("TODO.txt");
+    let mut todo = Todo::from(FILE);
     let mut ui = UI::new();
     let mut current_list = todo.collect_todo_md();
     let mut accepting_input = false;
@@ -31,6 +59,7 @@ fn main() {
     loop {
         if poll(Duration::from_millis(200)).unwrap() {
             if let Ok(event) = read() {
+                // handle inserting a new item
                 if accepting_input {
                     match event {
                         Event::Key(KeyEvent {
@@ -41,23 +70,47 @@ fn main() {
                         }) => {
                             accepting_input = false;
                             execute!(stdout, cursor::Hide).unwrap();
-                            execute!(stdout, MoveTo(0, current_list.len() as u16 + 2), terminal::Clear(terminal::ClearType::FromCursorDown)).unwrap();
-                        },
+                            execute!(
+                                stdout,
+                                MoveTo(0, current_list.len() as u16 + 2),
+                                terminal::Clear(terminal::ClearType::FromCursorDown)
+                            )
+                            .unwrap();
+                            new_todo_item = String::from("");
+                        }
                         Event::Key(KeyEvent {
                             code: KeyCode::Enter,
                             modifiers: KeyModifiers::NONE,
                             kind: KeyEventKind::Press,
                             ..
                         }) => {
-                            todo.add_to_todo(new_todo_item.clone());
+                            match ui.current_tab {
+                                Tabs::Done => {
+                                    todo.add_to_done(new_todo_item.clone());
+                                    current_list = todo.collect_done_md();
+                                }
+                                Tabs::Todo => {
+                                    todo.add_to_todo(new_todo_item.clone());
+                                    current_list = todo.collect_todo_md();
+                                }
+                                Tabs::All => {
+                                    todo.add_to_todo(new_todo_item.clone());
+                                    current_list = todo.collect_all_md();
+                                }
+                            }
                             new_todo_item.clear();
                             accepting_input = false;
                             execute!(stdout, cursor::Hide).unwrap();
-                            execute!(stdout, MoveTo(0, current_list.len() as u16 + 2), terminal::Clear(terminal::ClearType::FromCursorDown)).unwrap();
+                            execute!(
+                                stdout,
+                                MoveTo(0, current_list.len() as u16 + 2),
+                                terminal::Clear(terminal::ClearType::FromCursorDown)
+                            )
+                            .unwrap();
 
-                            current_list = todo.collect_todo_md();
                             ui.render_list(&current_list);
-                        },
+                        }
+                        // handle delete
                         Event::Key(KeyEvent {
                             code: KeyCode::Backspace,
                             modifiers: KeyModifiers::NONE,
@@ -66,9 +119,24 @@ fn main() {
                         }) => {
                             if !new_todo_item.is_empty() {
                                 new_todo_item.pop();
-                                execute!(stdout, MoveTo(8 + new_todo_item.len() as u16, current_list.len() as u16 + 2), Print(" "), MoveTo(8 + new_todo_item.len() as u16, current_list.len() as u16 + 2)).unwrap();
+                                clear_and_render(&mut stdout, current_list.len(), &new_todo_item);
                             }
-                        },
+                        }
+                        // handle control delete, delete the last word
+                        Event::Key(KeyEvent {
+                            code: KeyCode::Backspace,
+                            modifiers: KeyModifiers::CONTROL,
+                            kind: KeyEventKind::Press,
+                            ..
+                        }) => {
+                            if !new_todo_item.is_empty() {
+                                new_todo_item = remove_last_word(&new_todo_item);
+                                if new_todo_item.len() > 1 {
+                                    new_todo_item.push(' ');
+                                }
+                                clear_and_render(&mut stdout, current_list.len(), &new_todo_item);
+                            }
+                        }
                         Event::Key(KeyEvent {
                             code: KeyCode::Char(c),
                             modifiers: KeyModifiers::NONE,
@@ -76,11 +144,21 @@ fn main() {
                             ..
                         }) => {
                             new_todo_item.push(c);
-                            execute!(stdout, MoveTo(7 + new_todo_item.len() as u16, current_list.len() as u16 + 2), Print(c)).unwrap();
-                        },
-                        _ => {},
+                            clear_and_render(&mut stdout, current_list.len(), &new_todo_item);
+                        }
+                        Event::Key(KeyEvent {
+                            code: KeyCode::Char(c),
+                            modifiers: KeyModifiers::SHIFT,
+                            kind: KeyEventKind::Press,
+                            ..
+                        }) => {
+                            new_todo_item.push(c.to_ascii_uppercase());
+                            clear_and_render(&mut stdout, current_list.len(), &new_todo_item);
+                        }
+                        _ => {}
                     }
                 } else {
+                    // match keybinds
                     match event {
                         Event::Key(KeyEvent {
                             code: KeyCode::Char('q'),
@@ -89,7 +167,7 @@ fn main() {
                             ..
                         }) => {
                             break; // exit on q
-                        },
+                        }
                         Event::Key(KeyEvent {
                             code: KeyCode::Char('j'),
                             modifiers: KeyModifiers::NONE,
@@ -97,9 +175,9 @@ fn main() {
                             ..
                         }) => {
                             // handle move down
-                            ui.handle_input(Input::Down);
+                            ui.handle_input(Input::Down, current_list.len());
                             ui.render_list(&current_list);
-                        },
+                        }
                         Event::Key(KeyEvent {
                             code: KeyCode::Char('k'),
                             modifiers: KeyModifiers::NONE,
@@ -107,9 +185,17 @@ fn main() {
                             ..
                         }) => {
                             // handle move up
-                            ui.handle_input(Input::Up);
+                            ui.handle_input(Input::Up, current_list.len());
                             ui.render_list(&current_list);
-                        },
+                        }
+                        Event::Key(KeyEvent {
+                            code: KeyCode::Char('s'),
+                            modifiers: KeyModifiers::NONE,
+                            kind: KeyEventKind::Press,
+                            ..
+                        }) => {
+                            todo.save(FILE);
+                        }
                         Event::Key(KeyEvent {
                             code: KeyCode::Char('a'),
                             modifiers: KeyModifiers::NONE,
@@ -117,9 +203,57 @@ fn main() {
                             ..
                         }) => {
                             // handle append
-                            execute!(stdout, MoveTo(1, current_list.len() as u16 + 2), Print("- [ ]")).unwrap();
-                            execute!(stdout, MoveTo(7, current_list.len() as u16 + 2), cursor::Show, SetCursorStyle::BlinkingBar).unwrap();
+                            execute!(
+                                stdout,
+                                MoveTo(1, current_list.len() as u16 + 2),
+                                Print("- [ ]")
+                            )
+                            .unwrap();
+                            execute!(
+                                stdout,
+                                MoveTo(7, current_list.len() as u16 + 2),
+                                cursor::Show,
+                                SetCursorStyle::BlinkingBar
+                            )
+                            .unwrap();
                             accepting_input = true;
+                        }
+                        Event::Key(KeyEvent {
+                            code: KeyCode::Char('x'),
+                            modifiers: KeyModifiers::NONE,
+                            kind: KeyEventKind::Press,
+                            ..
+                        }) => match ui.current_tab {
+                            Tabs::Done => {
+                                todo.mark_as_todo(ui.position);
+                                current_list = todo.collect_done_md();
+                                ui.set_position(0);
+                                ui.render_list(&current_list);
+                            }
+                            Tabs::Todo => {
+                                todo.mark_as_done(ui.position);
+                                current_list = todo.collect_todo_md();
+                                ui.set_position(0);
+                                ui.render_list(&current_list);
+                            }
+                            Tabs::All => {
+                                let (local_pos, tab) = todo.all_to_single(ui.position);
+
+                                match tab {
+                                    Tabs::Todo => {
+                                        todo.mark_as_done(local_pos);
+                                        ui.set_position(0);
+                                        current_list = todo.collect_all_md();
+                                    }
+                                    Tabs::Done => {
+                                        todo.mark_as_todo(local_pos);
+                                        ui.set_position(0);
+                                        current_list = todo.collect_all_md();
+                                    }
+                                    _ => {}
+                                }
+                                ui.render_list(&current_list);
+                            }
                         },
                         Event::Key(KeyEvent {
                             code: KeyCode::Tab,
@@ -133,13 +267,13 @@ fn main() {
                                 Tabs::Todo => todo.collect_done_md(),
                                 Tabs::Done => todo.collect_all_md(),
                             };
-    
+
                             ui.set_position(0);
                             ui.cycle_tabs();
                             ui.render_list(&current_list);
                             ui.render_tabs();
                         }
-                        _ => {},
+                        _ => {}
                     }
                 }
             }
